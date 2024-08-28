d0f0b40ff575b1f57d0632159b2a45c2d0937baa
gerasantillan17 <gerapumas122@gmail.com>
2024-08-28 16:12:52 -0600

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int pulgadas;
+        double precio;
+        String tecpan;
+        
+        marca = "Samsung";
+        modelo = "Crystal UHD";
+        pulgadas = 75;
+        precio = 40000.1;
+        tecpan = "LED";
+                
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+        System.out.println(tecpan);
+    }
+    
+}
